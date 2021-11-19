4c04b5e49943e2e569c897573cacfb952d50f2d1
immiq <ayd@immiq.top>
2021-11-19 15:29:22 +0800

    fuck checkCollision in CircleObstacle.java

--- src/main/java/component/CircleObstacle.java
+++ src/main/java/component/CircleObstacle.java
@@ -1,5 +1,7 @@
 package component;
 
+import geometry.Circle;
+import geometry.Geometry;
 import geometry.Point;
 import java.util.Map;
 import javax.swing.*;
@@ -14,6 +16,8 @@ import utils.ComponentType;
  **/
 
 public class CircleObstacle extends NormalComponent {
+  private Circle circle;
+
   public CircleObstacle() {
     setType(ComponentType.CIRCLE);
     setLabel(new JLabel(ComponentImages.getImage(getType())));
@@ -21,7 +25,33 @@ public class CircleObstacle extends NormalComponent {
 
   @Override
   public Point checkCollision(Ball ball) {
-    return null;
+    double br = ball.getCircle().getRadius(), r = this.circle.getRadius(),
+           cx = this.circle.getCenter().getX(), cy = this.circle.getCenter().getY(),
+           px = ball.getCircle().getCenter().getX(), py = ball.getCircle().getCenter().getY();
+    if (Geometry.pointToPointDistance(this.circle.getCenter(), ball.getCircle().getCenter())
+        > br + r) {
+      return null;
+    } else {
+      double v2 = cx * cx - 2 * cx * px + cy * cy - 2 * cy * py + px * px + py * py;
+      double v1 =
+          Math.pow(cy, 3) / v2 + (cx * cx * cy) / v2 + (cy * px * px) / v2 + (cy * py * py) / v2;
+      double v3 = cy * r * Math.sqrt(1 / v2);
+      double v4 = py * r * Math.sqrt(1 / v2);
+      double v = v1 - (2 * cy * cy * py) / v2 + v3 - v4 - (2 * cx * cy * px) / v2;
+      Point res = new Point(-(cx * py - cy * px - cx * v + px * v) / (cy - py), v);
+      if (Geometry.pointToPointDistance(res, ball.getCircle().getCenter()) <= br) {
+        return res;
+      } else {
+        double v5 = (v1) - (2 * cy * cy * py) / v2 - v3 + v4 - (2 * cx * cy * px) / v2;
+        res.setX(-(cx * py - cy * px - cx * v5 + px * v5) / (cy - py));
+        res.setY(v5);
+        if (Geometry.pointToPointDistance(res, ball.getCircle().getCenter()) <= br) {
+          return res;
+        } else {
+          return null;
+        }
+      }
+    }
   }
 
   @Override
