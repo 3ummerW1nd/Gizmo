b518fa101c80457d6fed3c53d65827f0e71c8ded
immiq <ayd@immiq.top>
2021-11-25 13:24:23 +0800

    change StraightRail to two lines

--- src/main/java/component/rail/Rail.java
+++ src/main/java/component/rail/Rail.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @program: Gizmo
@@ -17,6 +18,7 @@ import lombok.Data;
  * @author: 3ummerW1nd
  * @create: 2021-10-31 19:40
  **/
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class Rail extends NormalComponent {
   static final byte LEFT = 0, TOP = 1, RIGHT = 2, BOTTOM = 3;
@@ -25,52 +27,15 @@ public class Rail extends NormalComponent {
   private Point upperLeft;
 
   @Override
-  public Point checkCollision(Ball ball) {
-    double radius = ball.getCircle().getRadius();
-    Point center = ball.getCircle().getCenter();
-    List<Line> lines = new ArrayList<>();
-    lines.add(new Line(upperLeft, upperLeft.add(new Point(sideLength, 0))));
-    lines.add(new Line(upperLeft, upperLeft.add(new Point(0, sideLength))));
-    lines.add(new Line(
-        upperLeft.add(new Point(sideLength, 0)), upperLeft.add(new Point(sideLength, sideLength))));
-    lines.add(new Line(
-        upperLeft.add(new Point(0, sideLength)), upperLeft.add(new Point(sideLength, sideLength))));
-    Line bestLine = null;
-    for (var line : lines) {
-      if (radius >= Geometry.pointToSegmentDistance(center, line)) {
-        radius = Geometry.pointToSegmentDistance(center, line);
-        bestLine = line;
-      }
-    }
-    if (bestLine == null) {
-      return null;
-    } else {
-      double dis = Geometry.pointToSegmentDistance(center, bestLine);
-      if (dis == Geometry.pointToPointDistance(center, bestLine.getS())) {
-        return bestLine.getS();
-      } else if (dis == Geometry.pointToPointDistance(center, bestLine.getT())) {
-        return bestLine.getT();
-      } else {
-        double ax = center.getX(), ay = center.getY(), sx = bestLine.getS().getX(),
-               sy = bestLine.getS().getY(), tx = bestLine.getT().getX(),
-               ty = bestLine.getT().getY();
-        double tmp = sx * sx - 2 * sx * tx + sy * sy - 2 * sy * ty + tx * tx + ty * ty;
-        return new Point((ax * sx * sx - sx * sy * ty + ay * sx * sy - 2 * ax * sx * tx
-                             + sx * ty * ty - ay * sx * ty + sy * sy * tx - sy * tx * ty
-                             - ay * sy * tx + ax * tx * tx + ay * tx * ty)
-                / tmp,
-            (sx * sx * ty - sx * sy * tx + ax * sx * sy - sx * tx * ty - ax * sx * ty + ay * sy * sy
-                + sy * tx * tx - ax * sy * tx - 2 * ay * sy * ty + ax * tx * ty + ay * ty * ty)
-                / tmp);
-      }
-    }
-  }
-
-  @Override
   public void init(Map.Entry<Integer, Integer> box) {
     super.init(box);
     this.upperLeft = new Point(box.getKey() * 30, box.getValue() * 30);
     sideLength = 30 * getSize();
+  }
+
+  @Override
+  public Point checkCollision(Ball ball) {
+    return null;
   }
 
   @Override
--- src/main/java/component/rail/StraightRail.java
+++ src/main/java/component/rail/StraightRail.java
@@ -1,5 +1,11 @@
 package component.rail;
 
+import component.Ball;
+import geometry.Geometry;
+import geometry.Line;
+import geometry.Point;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import javax.swing.*;
 import utils.ComponentImages;
@@ -17,6 +23,56 @@ public class StraightRail extends Rail {
     super();
     setType(ComponentType.STRAIGHT_RAIL);
     setLabel(new JLabel(ComponentImages.getImage(getType())));
+  }
+
+  @Override
+  public Point checkCollision(Ball ball) {
+    double radius = ball.getCircle().getRadius();
+    Point center = ball.getCircle().getCenter();
+    List<Line> lines = new ArrayList<>();
+    if (getEntranceX() == LEFT || getEntranceY() == LEFT) {
+      lines.add(new Line(getUpperLeft(), getUpperLeft().add(new Point(0, getSideLength()))));
+    }
+    if (getEntranceX() == TOP || getEntranceY() == TOP) {
+      lines.add(new Line(getUpperLeft(), getUpperLeft().add(new Point(getSideLength(), 0))));
+    }
+    if (getEntranceX() == RIGHT || getEntranceY() == RIGHT) {
+      lines.add(new Line(getUpperLeft().add(new Point(getSideLength(), getSideLength())),
+          getUpperLeft().add(new Point(getSideLength(), 0))));
+    }
+    if (getEntranceX() == BOTTOM || getEntranceY() == BOTTOM) {
+      lines.add(new Line(getUpperLeft().add(new Point(getSideLength(), getSideLength())),
+          getUpperLeft().add(new Point(0, getSideLength()))));
+    }
+    Line bestLine = null;
+    for (var line : lines) {
+      if (radius >= Geometry.pointToSegmentDistance(center, line)) {
+        radius = Geometry.pointToSegmentDistance(center, line);
+        bestLine = line;
+      }
+    }
+    if (bestLine == null) {
+      return null;
+    } else {
+      double dis = Geometry.pointToSegmentDistance(center, bestLine);
+      if (dis == Geometry.pointToPointDistance(center, bestLine.getS())) {
+        return bestLine.getS();
+      } else if (dis == Geometry.pointToPointDistance(center, bestLine.getT())) {
+        return bestLine.getT();
+      } else {
+        double ax = center.getX(), ay = center.getY(), sx = bestLine.getS().getX(),
+               sy = bestLine.getS().getY(), tx = bestLine.getT().getX(),
+               ty = bestLine.getT().getY();
+        double tmp = sx * sx - 2 * sx * tx + sy * sy - 2 * sy * ty + tx * tx + ty * ty;
+        return new Point((ax * sx * sx - sx * sy * ty + ay * sx * sy - 2 * ax * sx * tx
+                             + sx * ty * ty - ay * sx * ty + sy * sy * tx - sy * tx * ty
+                             - ay * sy * tx + ax * tx * tx + ay * tx * ty)
+                / tmp,
+            (sx * sx * ty - sx * sy * tx + ax * sx * sy - sx * tx * ty - ax * sx * ty + ay * sy * sy
+                + sy * tx * tx - ax * sy * tx - 2 * ay * sy * ty + ax * tx * ty + ay * ty * ty)
+                / tmp);
+      }
+    }
   }
 
   @Override
